f2110fc3fa230e5d0e0eca03399bdd0d49d91a70
Ioana Pasca <ioanapasca10@gmail.com>
2021-12-29 21:35:09 +0200

    fix:test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -7,7 +7,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
   <div>
-    <h1>Hi thereeeee!</h1>
+    <h1>Hi thereeeeeaaaß!</h1>
     <hr />
     <MarketingApp/>
   </div>
