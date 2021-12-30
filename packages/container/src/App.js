69345a7460cd795135dda16c011f5681b4782419
Ioana Pasca <ioanapasca10@gmail.com>
2021-12-30 19:42:10 +0200

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 // mount function take a reference to html element is nopt a react component
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
   return (
-  <div>
-    <h1>Hi there!!!!</h1>
-    <hr />
-    <MarketingApp/>
-  </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <MarketingApp/>
+      </div>
+    </BrowserRouter>
   )
 }
 
